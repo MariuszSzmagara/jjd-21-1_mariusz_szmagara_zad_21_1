5f34581786f40261b5ee90abc9128b7c80d3450c
MariuszSzmagara <mariusz.szmagara@gmail.com>
2021-03-30 10:40:26 +0200

    Initial commit

---
+++ src/test/java/pl/javastart/Jjd211MariuszSzmagaraZad211ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jjd211MariuszSzmagaraZad211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
